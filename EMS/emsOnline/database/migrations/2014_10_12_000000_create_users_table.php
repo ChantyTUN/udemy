162931a4aafc4e6c902d2f2e699bbd5b5e6563e8
chanty007 <cambodia4khmeru@gmail.com>
2020-09-24 16:00:51 +0700

    Basic Admin Layouts and tables

--- EMS/emsOnline/database/migrations/2014_10_12_000000_create_users_table.php
+++ EMS/emsOnline/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,13 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('mobile_number')->nullable();
+            $table->integer('department_id');
+            $table->integer('role_id');
+            $table->string('designation');
+            $table->date('start_from');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
--- EMS/emsOnline/resources/views/admin/layouts/master.blade.php
+++ EMS/emsOnline/resources/views/admin/layouts/master.blade.php
@@ -1,4 +1,9 @@
 @include('admin.layouts.navbar')
 @include('admin.layouts.sidebar')
-@include('admin.layouts.content')
+<div id="layoutSidenav_content">
+    <main>
+        @yield('content')
+
+    </main>
+
 @include('admin.layouts.footer')
